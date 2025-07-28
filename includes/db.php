8bfbaa28bb1a43c8ab59c92fad8dac5d6e7e1226
eunice <eunices.del@soy.sena.edu.co>
2025-07-28 16:56:36 -0500

    🔧 Ajuste de db.php para conexión con variables de entorno en Railway

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$clave = ""; // o tu contraseña si tienes una
-$bd = "stockea_db";
+$host = $_ENV['DB_HOST'];
+$usuario = $_ENV['DB_USER'];
+$clave = $_ENV['DB_PASSWORD'];
+$bd = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
 
-$conn = new mysqli($host, $usuario, $clave, $bd);
+$conn = new mysqli($host, $usuario, $clave, $bd, $port);
 
 if ($conn->connect_error) {
     die("❌ Error de conexión: " . $conn->connect_error);
